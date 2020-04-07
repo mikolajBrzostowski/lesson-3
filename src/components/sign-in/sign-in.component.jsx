3059b079dce43ad213750c83221f54402b4a297a
mikolaj.brzostowski@gmail.com <mikolaj.brzostowski@gmail.com>
2020-04-07 05:23:09 +0200

    section 8 - adding redux

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import './sign-in.component.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
+import FormInput from './../form-input/form-input.component';
+
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { email, password  } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.error(error);
+    }
+    
+  };
+
+  handleChange = (e) => {
+    const { value, name } = e;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { email, password  } = this.state;
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+        <FormInput
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            label="Password"
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In With Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
